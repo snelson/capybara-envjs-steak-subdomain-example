c70df93ad7e758c8fca96f659be4bbd2971de799
Scott Nelson <scott@thisbythem.com>
2011-03-23 23:52:24 -0700

    New rails app without test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapybaraEnvjsSteakSubdomainExample::Application.config.secret_token = 'bdae7923053464a6fac17ab560a3d7945a821a4749257a4f7607dbfa700dd454c73fd9c6b02faa8d21568ae4af1687116f564645f7d76e0eeee6c68826e29ee7'
