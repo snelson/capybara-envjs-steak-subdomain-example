f884771d1edf65afb05c987ed126bbf68bb05ab5
Scott Nelson <scott@thisbythem.com>
2011-03-24 20:44:06 -0700

    Switch subdomain using visit() as Jonas recommends

--- spec/acceptance/subdomains_spec.rb
+++ spec/acceptance/subdomains_spec.rb
@@ -2,7 +2,6 @@ require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 
 feature "Subdomains" do
   background do
-    Capybara.current_driver = :envjs
   end
 
   scenario "Access default domain" do
@@ -12,18 +11,22 @@ feature "Subdomains" do
   end
 
   scenario "Access subdomain one" do
-    Capybara.default_host = "one.example.com"
-    Capybara.app_host     = "http://one.example.com"
-    visit '/'
+    visit "http://one.example.com"
     current_url.should == "http://one.example.com/" # passes
-    find("h1").text.should == "http://one.example.com/" # fails with envjs
+    find("h1").text.should == "http://one.example.com/" # passes
+
+    visit '/'
+    current_url.should == "http://one.example.com/" # fails
+    find("h1").text.should == "http://one.example.com/" # fails
   end
 
   scenario "Access subdomain two" do
-    Capybara.default_host = "two.example.com"
-    Capybara.app_host     = "http://two.example.com"
-    visit '/'
+    visit 'http://two.example.com/'
     current_url.should == "http://two.example.com/" # passes
-    find("h1").text.should == "http://two.example.com/" # fails with envjs
+    find("h1").text.should == "http://two.example.com/" # passes
+
+    visit '/'
+    current_url.should == "http://two.example.com/" # fails
+    find("h1").text.should == "http://two.example.com/" # fails
   end
 end
